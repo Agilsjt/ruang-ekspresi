b45553ba6d2032ea5225695a570e34baae52b9ad
unknown <Agilsjt>
2025-12-12 23:20:35 +0700

    Configure base path for Vite deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+// vite.config.ts
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/ruang-ekspresi/', 
+  
   plugins: [react(), tailwindcss()],
 })
